c7af8049544d581b599a82b6d2785d1ec2cf99a2
Gimmy <975402925@qq.com>
2024-05-29 18:19:24 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonDemo from './components/ButtonDemo.vue'
+import AlertDemo from './components/AlertDemo.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="container">
+      <button-demo></button-demo>
+    </div>
+    <div class="container">
+      <alert-demo></alert-demo>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  width: 600px;
+  padding: 20px 24px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  margin-top: 24px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.container:first-child {
+  padding-bottom: 0px;
 }
 </style>
