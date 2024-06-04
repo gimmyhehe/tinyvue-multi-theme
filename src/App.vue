69ff965f32bc91a1b713047c072d9b172818c198
Gimmy <975402925@qq.com>
2024-06-04 19:03:58 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,51 @@
 import ButtonDemo from "./components/ButtonDemo.vue";
 import AlertDemo from "./components/AlertDemo.vue";
 import ChangeTheme from "./components/ChangeTheme.vue";
+import { ConfigProvider } from "@opentiny/vue";
+import { ref } from 'vue'
+
+const customDesign = {
+  name: "customDesign", // 自定义规范名称
+  components: {
+    Alert: {
+      icons: {
+        warning: () => null,
+        success: () => null,
+        error: () => null,
+      },
+      // 可以通过renderless重写某个方法，自定义交互逻辑
+      renderless: (props, hooks, { emit }, api) => {
+        const state = api.state;
+        return {
+          handleClose() {
+            const isClose = confirm('是否确认关闭alert')
+            if (isClose) {
+              state.show = false;
+            }
+          },
+        };
+      },
+    },
+  },
+};
+const design = ref({});
+const changeDesign = (theme) => {
+  design.value = theme === 'element' ? customDesign : {}
+};
 </script>
 
 <template>
-  <change-theme></change-theme>
-  <div class="theme-app">
-    <div class="container">
-      <button-demo></button-demo>
+  <change-theme @change-theme="changeDesign"></change-theme>
+  <config-provider :design="design" :key="design.name">
+    <div class="theme-app">
+      <div class="container">
+        <button-demo></button-demo>
+      </div>
+      <div class="container">
+        <alert-demo></alert-demo>
+      </div>
     </div>
-    <div class="container">
-      <alert-demo></alert-demo>
-    </div>
-  </div>
+  </config-provider>
 </template>
 
 <style scoped>
--- src/components/AlertDemo.vue
+++ src/components/AlertDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <tiny-alert type="success" description="type 为 success"></tiny-alert>
-    <tiny-alert description="type 为默认值 info"></tiny-alert>
+    <tiny-alert type="simple" description="type 为默认值 info"></tiny-alert>
     <tiny-alert type="warning" description="type 为 warning"></tiny-alert>
     <tiny-alert type="error" description="type 为 error"></tiny-alert>
   </div>
