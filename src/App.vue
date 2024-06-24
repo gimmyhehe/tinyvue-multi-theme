68e8929a08690a6676ebd5bef6c30e2c8b0fa306
gimmy <975402925@qq.com>
2024-06-25 00:19:01 +0800

    tradition-change

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,12 @@ const changeTheme = () => {
 </script>
 
 <template>
-  <div id="theme-app" :data-mode="theme">
-    <button class="btn" @click="changeTheme">
-      {{ theme === "light" ? "亮色模式" : "暗黑模式" }}
-    </button>
-    <p class="title">主题切换</p>
+  <div id="root" :data-mode="theme">
+    <div id="theme-app">
+      <button class="btn" @click="changeTheme">
+        {{ theme === "light" ? "亮色模式" : "暗黑模式" }}
+      </button>
+      <p class="title">主题切换</p>
+    </div>
   </div>
 </template>
