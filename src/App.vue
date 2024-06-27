b6ab250f3561d24f86563bdffd5d6794992396aa
gimmy <975402925@qq.com>
2024-06-27 18:36:34 +0800

    x-design-theme

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,19 @@
 import ButtonDemo from "./components/ButtonDemo.vue";
 import AlertDemo from "./components/AlertDemo.vue";
 import ChangeTheme from "./components/ChangeTheme.vue";
+import { ConfigProvider } from "@opentiny/vue";
+import { ref } from "vue";
+import designSmbConfig from "@opentiny/vue-design-smb";
+const designConfig = ref({});
+const changeDesign = (val) => {
+  console.log(val === "x-design");
+  designConfig.value = val === "x-design" ? designSmbConfig : {};
+};
 </script>
 
 <template>
-  <change-theme></change-theme>
-  <config-provier>
+  <change-theme @change-theme="changeDesign"></change-theme>
+  <config-provider :design="designConfig" :key="designConfig.name">
     <div class="theme-app">
       <div class="container">
         <button-demo></button-demo>
@@ -15,12 +23,12 @@ import ChangeTheme from "./components/ChangeTheme.vue";
         <alert-demo></alert-demo>
       </div>
     </div>
-  </config-provier>
+  </config-provider>
 </template>
 
 <style scoped>
 .container {
-  width: 650px;
+  width: 700px;
   padding: 20px 24px;
   border-radius: 5px;
   border: 1px solid #ddd;
--- src/components/ChangeTheme.vue
+++ src/components/ChangeTheme.vue
@@ -1,56 +1,25 @@
-<template>
-  <tiny-radio-group v-model="theme" @change="changeTheme">
-    <tiny-radio label="tiny-vue"></tiny-radio>
-    <tiny-radio label="element"></tiny-radio>
-  </tiny-radio-group>
-</template>
-
 <script setup>
 import {
   Radio as TinyRadio,
   RadioGroup as TinyRadioGroup,
 } from "@opentiny/vue";
-import TinyThemeTool from "../theme-tool";
+import TinyThemeTool from "@opentiny/vue-theme/theme-tool";
 import { ref } from "vue";
-
-const emit = defineEmits(["change-theme"]);
+import { tinySmbTheme } from "@opentiny/vue-theme/theme";
 
 const theme = ref("tiny-vue");
-
 const themeTool = new TinyThemeTool();
 
-const tinyTestTheme = {
-  id: "tiny-test-theme",
-  data: {
-    // 设置公共变量
-    "ti-common-color-primary-bg": "#409eff",
-    "ti-common-color-primary-normal": "#409eff",
-    "ti-common-color-success-bg": "#f0f9eb",
-    "ti-common-color-success-normal": "#67c23a",
-    "ti-common-color-info-bg": "#f4f4f5",
-    "ti-common-color-info-normal": "#909399",
-    "ti-common-color-warn-bg": "#fdf6ec",
-    "ti-common-color-warn-normal": "#e6a23c",
-    "ti-common-color-danger-bg": "#fef0f0",
-    "ti-common-color-danger-normal": "#f56c6c",
-    // 设置button相关样式属性
-    "ti-button-size-normal-height": "32px",
-    "ti-button-border-radius": "4px",
-    "ti-button-font-size": "14px",
-    // 设置alert相关样式属性
-    "ti-alert-border-radius": "6px",
-    "ti-alert-close-font-size": "14px",
-    "ti-alert-description-font-size": "14px",
-    "ti-alert-success-close-icon-color": "#a8abb2",
-    "ti-alert-info-close-icon-color": "#a8abb2",
-    "ti-alert-error-close-icon-color": "#a8abb2",
-    "ti-alert-warning-close-icon-color": "#a8abb2",
-    "ti-alert-border-weight": "0px",
-  },
-};
-
+const emit = defineEmits(["change-theme"]);
 const changeTheme = (theme) => {
-  themeTool.changeTheme(theme === "element" ? tinyTestTheme : { data: {} });
+  themeTool.changeTheme(theme === "x-design" ? tinySmbTheme : null);
   emit("change-theme", theme);
 };
 </script>
+
+<template>
+  <tiny-radio-group v-model="theme" @change="changeTheme">
+    <tiny-radio label="tiny-vue"></tiny-radio>
+    <tiny-radio label="x-design"></tiny-radio>
+  </tiny-radio-group>
+</template>
